89703fae8f0bce1ffc824833b1abac01c7c1219c
Robert Rosolek <robertrosolek@gmail.com>
2013-05-04 20:33:02 +0200

    get rid of #defines in template

--- template.cpp
+++ template.cpp
@@ -4,7 +4,6 @@
 
 #include <iomanip>
 #include <iostream>
-#include <fstream>
 #include <sstream>
 #include <utility>
 #include <algorithm>
@@ -14,20 +13,7 @@
 #include <queue>
 #include <set>
 #include <map>
-#include <list>
-#include <stack>
 using namespace std;
-
-#define ALL(x) x.begin(), x.end()
-#define VAR(a,b) __typeof (b) a = b
-#define REP(i,n) for (int _n=(n), i=0; i<_n; ++i)
-#define FOR(i,a,b) for (int _b=(b), i=(a); i<=_b; ++i)
-#define FORD(i,a,b) for (int _b=(b), i=(a); i>=_b; --i)
-#define FORE(i,a) for (VAR(i,a.begin ()); i!=a.end (); ++i) 
-#define PB push_back
-#define MP make_pair
-#define ST first
-#define ND second
 
 typedef vector<int> VI;
 typedef long long LL;

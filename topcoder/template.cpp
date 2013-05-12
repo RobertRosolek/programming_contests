662f0e9bc59b7d23f7f4dd64ba4281cd8dea3620
Robert Rosolek <robertrosolek@gmail.com>
2013-05-12 18:13:14 +0200

    add stl containers headers to template

--- topcoder/template.cpp
+++ topcoder/template.cpp
@@ -5,11 +5,20 @@
 #include <cassert>
 #include <string>
 #include <vector>
+#include <queue>
+#include <set>
+#include <map>
+
 using namespace std;
 
 typedef vector<int> VI;
 typedef long long LL;
 typedef pair<int,int> PII;
+
+/* CHECKLIST 
+ * 1) long longs */
+
+const int DBG = 0, INF = int(1e9);
 
 class <CLASSNAME> {
 	public:

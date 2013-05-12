19271f72f59567628b0e2508e52735dd8fe5b7a5
Robert Rosolek <robertrosolek@gmail.com>
2013-05-12 18:12:52 +0200

    clean up template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,3 @@
-#include <stdio.h>      
-#include <ctype.h>
-#include <math.h>
-
 #include <iomanip>
 #include <iostream>
 #include <sstream>
@@ -13,6 +9,7 @@
 #include <queue>
 #include <set>
 #include <map>
+
 using namespace std;
 
 typedef vector<int> VI;

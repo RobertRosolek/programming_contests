885e79e21b25917944d06ec1b9c6890889dd1497
Robert Rosolek <robertrosolek@gmail.com>
2013-11-01 20:53:30 +0100

    TopCoder SRM 596, fix bug in 500 - long long issues

--- topcoder/SRM-596/500/A.cpp
+++ topcoder/SRM-596/500/A.cpp
@@ -21,31 +21,32 @@ public:
 
    		set<int> fb; for (int i = 0; i < B; ++i) fb.insert(i);
    		vector<set<int>> av(N);
-   		for (int i = 0; i < m; ++i) for (int j = 0; j < B; ++j) if (subset[i]&(1<<j)) { fb.erase(j); av[i].insert(j); }
+   		for (int i = 0; i < m; ++i) for (int j = 0; j < B; ++j) if (subset[i]&(1LL<<j)) { fb.erase(j); av[i].insert(j); }
 
    		for (int a = 0; a < m; ++a) for (int b = 0; b < m; ++b) if (a != b) {
-   			int nd = subset[a] & subset[b];
+   			long long nd = subset[a] & subset[b];
    			if (nd == 0) return vector<long long>();
-   			for (int j = 0; j < B; ++j) if (nd & (1<<j)) av[a].erase(j), av[b].erase(j);
+   			for (int j = 0; j < B; ++j) if (nd & (1LL<<j)) av[a].erase(j), av[b].erase(j);
    		}
    		for (auto x: subset) for (auto y: subset) if (x != y) for (auto z: subset) if (x != z && y != z && (x&y&z) != 0) return vector<long long>();
    		for (; m < N; ++m) {
-   			int k = 0;
+   			long long k = 0;
    			for (int i = 0; i < m; ++i) {
    				if (av[i].empty()) return vector<long long>();
    				int j = *av[i].begin(); av[i].erase(av[i].begin());
-   				k |= 1<<j;
+   				k |= 1LL<<j;
    			}
    			int rem = N -m - 1;
    			if (fb.size() < rem) return vector<long long>();
    			for (int q = 0; q < rem; ++q) {
    				int j = *fb.begin(); fb.erase(fb.begin());
    				av[m].insert(j);
-   				k |= 1<<j;
+   				k |= 1LL<<j;
    			}
    			res.push_back(k);
    		}
-
+   		for (auto x: res) for (auto y: res) if (x != y) for (auto z: res) if (x != z && y != z && (x&y&z) != 0) assert(false);
+   		for (auto x: res) for (auto y: res) if ((x&y) == 0) assert(false);
    		sort(res.begin(), res.end());
    		return res;
 	}

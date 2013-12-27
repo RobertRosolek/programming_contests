711224ed79f3b6f5c5fbca75f667823cf5d7e54b
Robert Rosolek <robertrosolek@gmail.com>
2013-12-27 10:04:25 +0100

    change flow algorithm - remove constant dependancy, make nicer with c++11

--- library_cpp/flow.cpp
+++ library_cpp/flow.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>      
+#include <stdio.h>
 #include <ctype.h>
 #include <math.h>
 
@@ -22,7 +22,7 @@ using namespace std;
 #define REP(i,n) for (int _n=(n), i=0; i<_n; ++i)
 #define FOR(i,a,b) for (int _b=(b), i=(a); i<=_b; ++i)
 #define FORD(i,a,b) for (int _b=(b), i=(a); i>=_b; --i)
-#define FORE(i,a) for (VAR(i,a.begin ()); i!=a.end (); ++i) 
+#define FORE(i,a) for (VAR(i,a.begin ()); i!=a.end (); ++i)
 #define PB push_back
 #define MP make_pair
 #define ST first
@@ -31,80 +31,71 @@ using namespace std;
 typedef vector<int> VI;
 typedef long long LL;
 
-
 const int DBG = 0, INF = 1000000000;
 
-const int MAXN = 1000;
-
-
+#include <queue>
 struct Graph {
+   int n;
+   vector<vector<int>> V;
 
-  int N;
-  VI V[MAXN];
+   vector<vector<int>> przepust;
+   vector<int> dst;
+   vector<vector<int>::iterator> pocz;
 
-  int przepust[MAXN][MAXN], dst[MAXN], q[MAXN];
-  vector<int>::iterator pocz[MAXN];
-
-
-  Graph(int N) : N(N) {
-    REP(i,N) REP(j,N)
-      przepust[i][j] = 0;
-  }
-
-
-  void addEdge(int a, int b, int d = INF) {
-    przepust[a][b] += d;
-    V[a].PB(b);
-    V[b].PB(a);
-  }
-
-
-  int flowBfs(int s, int t) {
-    REP(i,N)
-      dst[i] = -1;
-    dst[s] = 0;
-    int left = 0, right = 0;
-    q[right++] = s;
-    while (left < right) {
-      int k = q[left++];
-      FORE(it,V[k])
-        if (przepust[k][*it] > 0 && dst[*it] == -1) {
-          dst[*it] = dst[k] + 1;
-          q[right++] = *it;
-        }
-    }
-    return dst[t] != -1;
-  }
-
-  int flowDfs(int x,int t,int minimum){
-    int res = 0;
-    if (x == t || minimum == 0) return minimum;
-    for(vector<int>::iterator &it = pocz[x]; it != V[x].end(); ++it){
-      if (dst[x] + 1 == dst[*it] && przepust[x][*it] > 0) {
-        int y = flowDfs(*it, t, min(minimum, przepust[x][*it]));
-        przepust[x][*it] -= y; 
-        przepust[*it][x] += y;
-        minimum -= y; 
-        res += y;
-        if (minimum == 0) 
-          break;
+   int flowBfs(int s, int t) {
+      fill(dst.begin(), dst.end(), -1);
+      dst[s] = 0;
+      queue<int> q;
+      q.push(s);
+      while (!q.empty()) {
+         int u = q.front(); q.pop();
+         for (auto v: V[u])
+            if (przepust[u][v] > 0 && dst[v] == -1) {
+               dst[v] = dst[u] + 1;
+               q.push(v);
+            }
       }
-    }
-    return res;
-  }
-  
-  int maxFlow(int s, int t){
-    int res = 0;
-    while (flowBfs(s,t)){
-     REP(i,N) 
-       pocz[i]=V[i].begin();
-     res += flowDfs(s,t,INF);
-    }
-    return res;
-  }
+      return dst[t] != -1;
+   }
+
+   int flowDfs(int x, int t, int minimum) {
+      int res = 0;
+      if (x == t || minimum == 0) return minimum;
+      for (auto &it = pocz[x]; it != V[x].end(); ++it) {
+         if (dst[x] + 1 == dst[*it] && przepust[x][*it] > 0) {
+            int y = flowDfs(*it, t, min(minimum, przepust[x][*it]));
+            przepust[x][*it] -= y;
+            przepust[*it][x] += y;
+            minimum -= y;
+            res += y;
+            if (minimum == 0) break;
+         }
+      }
+      return res;
+   }
+
+   public:
+
+   Graph(int n) : n(n) , V(vector<vector<int>>(n)), przepust(vector<vector<int>>(n, vector<int>(n, 0))),
+      dst(vector<int>(n)), pocz(vector<vector<int>::iterator>(n)) {}
+
+   void addEdge(int a, int b, int d = INF) {
+      assert(a < n && b < n);
+      przepust[a][b] += d;
+      V[a].push_back(b);
+      V[b].push_back(a);
+   }
 
 
-  
+   int maxFlow(int s, int t) {
+      int res = 0;
+      while (flowBfs(s,t)) {
+         for (int i = 0; i < n; ++i) pocz[i]=V[i].begin();
+         res += flowDfs(s,t,INF);
+      }
+      return res;
+   }
+
 };
 
 int main() {

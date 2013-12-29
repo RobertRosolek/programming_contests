7c4ca135e1d412bef8ea1481c7cedcf4b6ae638e
Robert Rosolek <robertrosolek@gmail.com>
2013-12-29 19:57:30 +0100

    codeforces round 222, correct solution to A

--- codeforces/222/A/A.cpp
+++ codeforces/222/A/A.cpp
@@ -19,39 +19,36 @@ const int DBG = 0, INF = int(1e9);
 
 const int dx[] = {-1,1,0,0}, dy[] = {0,0,-1,1};
 
+int n, m, k, ch;
+vector<string> b;
+int vis[500 + 2][500 + 2];
+
+void dfs(int x, int y) {
+   if (b[x][y] != '.' || vis[x][y]) return;
+   vis[x][y] = true;
+   for (int i = 0; i < 4; ++i) dfs(x + dx[i], y + dy[i]);
+   if (ch == k) return;
+   ++ch;
+   b[x][y] = 'X';
+}
+
 int main() {
    ios_base::sync_with_stdio(0);
    cout.setf(ios::fixed);
 
-   int n, m, k; cin >> n >> m >> k;
-   vector<string> b(n + 2);
+   cin >> n >> m >> k;
+   b.resize(n + 2);
    for (int i = 0; i < m + 2; ++i) b[0] += '#';
    b[n + 1] = b[0];
    for (int i = 1; i <= n; ++i) {
       string line; cin >> line;
       b[i] = "#" + line + "#";
    }
-   int deg[n + 2][m + 2]; for (int a = 0; a < n + 2; ++a) for (int b = 0; b < m + 2; ++b) deg[a][b] = 0;
-   set<pair<int, pair<int, int>>> s;
-   for (int i = 1; i <= n; ++i) for (int j = 1; j <= m; ++j) if (b[i][j] == '.') {
-      for (int q = 0; q < 4; ++q) {
-         int x = i + dx[q], y = j + dy[q];
-         if (b[x][y] == '.') deg[i][j]++;
-      }
-      s.insert(make_pair(deg[i][j], make_pair(i, j)));
-   }
-   for (int i = 0; i < k; ) {
-      int dg = s.begin()->first, x = s.begin()->second.first, y = s.begin()->second.second; s.erase(*s.begin());
-      if (dg > deg[x][y]) continue;
-      ++i;
-      b[x][y] = 'X';
-      for (int q = 0; q < 4; ++q) {
-         int nx = x + dx[q], ny = y + dy[q];
-         if (b[nx][ny] != '.') continue;
-         deg[nx][ny]--;
-         s.insert(make_pair(deg[nx][ny], make_pair(nx, ny)));
-      }
-   }
+   int x, y;
+   for (int i = 1; i <= n; ++i) for (int j = 1; j <= m; ++j) if (b[i][j] == '.') x = i, y = j;
+   for (int i = 0; i < n + 2; ++i) for (int j = 0; j < m + 2; ++j) vis[i][j] = false;
+   ch = 0;
+   dfs(x, y);
    for (int i = 1; i <= n; ++i) cout << b[i].substr(1, m) << endl;
 
    return 0;

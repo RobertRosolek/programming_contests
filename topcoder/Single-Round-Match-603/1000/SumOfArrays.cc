ef8a8bde66735cdb91d46d03c2fd496b9b4eadfa
Robert Rosolek <robertrosolek@gmail.com>
2014-11-23 14:41:06 -0500

    topcoder: SRM 603

---
+++ topcoder/Single-Round-Match-603/1000/SumOfArrays.cc
@@ -0,0 +1,26 @@
+
+// {{{ Boilerplate Code <--------------------------------------------------
+// vim:filetype=cpp:foldmethod=marker:foldmarker={{{,}}}
+
+#include <iostream>
+#include <sstream>
+#include <utility>
+#include <algorithm>
+#include <cassert>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+
+using namespace std;
+
+// }}}
+
+class SumOfArrays {
+public:
+   string findbestpair(int n, vector <int> Aseed, vector <int> Bseed) {
+      return string();
+   }
+};
+
